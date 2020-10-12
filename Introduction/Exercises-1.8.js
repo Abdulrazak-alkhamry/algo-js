1522386a238e6b15896ec2bf57e19a704895dded
Abdulrazak-alkhamry <70669257+Abdulrazak-alkhamry@users.noreply.github.com>
2020-10-12 14:48:50 +0200

    Introuduction done

--- Introduction/Exercises-1.8.js
+++ Introduction/Exercises-1.8.js
@@ -1,13 +1,22 @@
 const readlineSync = require("readline-sync");
-
+let story = ""
+let questionsNumb = 3
 
 let A = readlineSync.question('What would a clock look like with no numbers?');
-console.log('Timeless :)');
-console.log('2 questions left');
+questionsNumb = questionsNumb - 1
+console.log(questionsNumb)
+story = story + A
 
 
 let B = readlineSync.question('How do you make 7 an even number?');
-console.log('1 questions left');
+questionsNumb = questionsNumb - 1
+console.log(questionsNumb)
+story = story + B
 
 let c = readlineSync.question('If you had 4 apples and 5 oranges in one hand and 6 apples and 7 oranges in the other, what would you have?');
-console.log('0 questions left');
+questionsNumb = questionsNumb - 1
+console.log(questionsNumb)
+story = story + c
+
+
+console.log(story)
